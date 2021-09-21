6f5a50742c86a7037816b8173069446bda9b1ac5
Hugo-Baraer <hugo.baraer@mail.mcgill.ca>
2021-09-21 10:15:43 -0400

    added a folder for relevant generated figures

---
+++ FFT_overdensity.py
@@ -0,0 +1,11 @@
+"""
+  /path/to/my/file/%(dir_id)s/is/here/%(file_id)s
+ 
+  Author : Hugo Baraer
+  Affiliation : McGill University
+  Date of creation : 2021-09-21
+  
+  This module ....
+  
+"""
+ 
