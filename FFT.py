fixed kradius in the linear bias computation, fixed error computation in the same bias, and made the MCMC algorithm work out

FFT.py
@@ -35,7 +35,7 @@ def compute_fft(field,delta,box_dim):
     fft_shifted = np.fft.fftshift(np.fft.fftn(np.fft.fftshift(field)))
     X, Y = np.meshgrid(freqs, freqs)
 
-    return X, Y, fft_shifted
+    return X, Y, fft_shifted, min(freqs)*-1
 
 def plot_ftt_field(field,slice,X,Y, title = ''):
     '''

Gaussian_testing.py
@@ -38,7 +38,7 @@ def generate_gaussian_field(dim):
     covariance = np.diag(sigma ** 2)
     zi = multivariate_normal.pdf(xyz, mean=mu, cov=covariance)
     zi2 = np.reshape(zi, (x.shape))
-    print(zi2)
+    #print(zi2)
     # Reshape back to a (30, 30) grid.
     return zi2, mu[0], sigma[0]
 

project_driver.py
@@ -28,13 +28,13 @@ import statistical_analysis as sa
 #import pymks
 
 #adjustable parameters to look out before running the driver
-box_dim = 51 #the desired spatial resolution of the box #the redshift z_bar at which the over-density is computed
+box_dim = 50 #the desired spatial resolution of the box #the redshift z_bar at which the over-density is computed
 radius_thick = 3. #the radii thickness (will affect the number of bins
-
+box_len = 300 #default value of 300
 
 
 #intialize a coeval cube at red shift z = z\bar
-coeval = p21c.run_coeval(redshift=8.0,user_params={'HII_DIM': box_dim, "USE_INTERPOLATION_TABLES": False})
+coeval = p21c.run_coeval(redshift=8.0,user_params={'HII_DIM': box_dim, 'BOX_LEN': box_len, "USE_INTERPOLATION_TABLES": False})
 
 
 
@@ -65,31 +65,31 @@ X, Y, fft_gaussian_shifted = gauss.gaussian_fft(gaussian_field,delta,box_dim)
 
 
 #Compute the reionization redshift from the module z_re
-coeval.z_re_box = zre.generate_zre_field(16, 1, 1, coeval.z_re_box.shape[0])
+coeval.z_re_box = zre.generate_zre_field(16, 1, 1, coeval.z_re_box.shape[0],box_len)
 overzre, zre_mean = zre.over_zre_field(coeval.z_re_box)
 
 #Take and plot the Fourrier transform of the over-redshift along with it's frequnecy
 delta = coeval.user_params.BOX_LEN / coeval.user_params.DIM
-Xz, Yz, overzre_fft= FFT.compute_fft(overzre, delta, box_dim)
+Xz, Yz, overzre_fft, freqzre= FFT.compute_fft(overzre, delta, box_dim)
 
 #plot this F(over_zre(x))
 FFT.plot_ftt_field(overzre_fft, int(box_dim//2), Xz, Yz, title = r'F($\delta_z$ (x)) at a pixel dimension of {}³'.format(box_dim))
 
 
-coeval = p21c.run_coeval(redshift=zre_mean,user_params={'HII_DIM': box_dim, "USE_INTERPOLATION_TABLES": False})
+coeval = p21c.run_coeval(redshift=zre_mean,user_params={'HII_DIM': box_dim, 'BOX_LEN': box_len, "USE_INTERPOLATION_TABLES": False})
 
-Xd, Yd, overd_fft= FFT.compute_fft(coeval.density, delta, box_dim)
+Xd, Yd, overd_fft, freqd = FFT.compute_fft(coeval.density, delta, box_dim)
 #FFT.plot_ftt_field(overd_fft, int(box_dim//2), Xd, Yd, title = r'F($\delta_m$ (x)) at a redshift of {} and a pixel dimension of {}³'.format(coeval.redshift,box_dim))
 freqs = np.fft.fftshift(np.fft.fftfreq(box_dim, d=delta))
 
 #polar_div = np.ones((box_dim,box_dim,box_dim))
 #polar_div = sa.cart2sphA(division)
+overd_fft = np.square(abs(overd_fft))
+overzre_fft = np.square(abs(overzre_fft))
 
 
-
-cx = int(box_dim//2)
-cy = int(box_dim//2)
-
+#plot the power of the field
+FFT.plot_ftt_field(overzre_fft, int(box_dim//2), Xz, Yz, title = r'$|F(\delta_z (x))|^2$ at a pixel dimension of {}³'.format(box_dim))
 #wanted radius for plotting
 
 #mask = (x[np.newaxis,:]-cx)**2 + (y[:,np.newaxis]-cy)**2 == r**2
@@ -98,9 +98,12 @@ cy = int(box_dim//2)
 cx = int(box_dim//2)
 cy = int(box_dim//2)
 radii = np.linspace(0,np.sqrt(3*(cx)**2),num = int(np.sqrt(3*(cx)**2)/int(radius_thick)))
-values = np.zeros(len(radii), dtype = 'complex')
+kvalues = np.linspace(0,np.sqrt(3*(freqd)**2), num = int(np.sqrt(3*(cx)**2)/int(radius_thick)))
+kvalues = kvalues[1:]
+radii = radii[1:]
+values = np.zeros(len(radii))
 count = np.zeros(len(radii))
-values_overd = np.zeros(len(radii), dtype = 'complex')
+values_overd = np.zeros(len(radii))
 count_overd= np.zeros(len(radii))
 
 #loop through each point and seperate them in
@@ -127,8 +130,8 @@ for i in tqdm(range(box_dim), 'co'):
             for step, radius in enumerate(radii):
                 if k_radius < radius:
                     average = values_overd[step]/count_overd[step]
-                    sigmad[step] += abs((overd_fft[i, j, z] - average))**2
-                    sigmazre[step] += abs((overzre_fft[i, j, z] - (values[step]/count[step])))**2
+                    sigmad[step] += (overd_fft[i, j, z] - average)**2
+                    sigmazre[step] += (overzre_fft[i, j, z] - (values[step]/count[step]))**2
                     break
 
             # if step == len(radii)-1:
@@ -142,7 +145,7 @@ sigmazre = np.sqrt(np.divide(sigmazre,count))
 
 print(values)
 print(count)
-
+print(sigmazre)
 overzre_fft_k = np.divide(values,count)
 overd_fft_k = np.divide(values_overd,count_overd)
 
@@ -164,13 +167,7 @@ overd_fft_k = np.divide(values_overd,count_overd)
 
 
 fig, ax = plt.subplots()
-plt.errorbar(radii, overd_fft_k, yerr = sigmad, linestyle = 'None',capsize=4, marker ='o')
-
-#plt.scatter(freqs, a[25], label = '25')
-# plt.scatter(freqs, a[50], label = '50')
-# plt.scatter(freqs, a[95], label = '95')
-#plt.contourf(Xd,Yd,a)
-#plt.colorbar()
+plt.errorbar(kvalues, overd_fft_k, yerr = sigmad, linestyle = 'None',capsize=4, marker ='o')
 #plt.legend()
 ax.set_xlabel(r'$k [Mpc^{-1}]$')
 ax.set_ylabel(r'$\delta_m$ (k)')
@@ -179,82 +176,73 @@ plt.show()
 
 
 fig, ax = plt.subplots()
-plt.errorbar(radii, overzre_fft_k, yerr = sigmazre, linestyle = 'None',capsize=4, marker ='o')
-#plt.scatter(radii, overzre_fft_k, )
-
-#plt.scatter(freqs, a[25], label = '25')
-# plt.scatter(freqs, a[50], label = '50')
-# plt.scatter(freqs, a[95], label = '95')
-#plt.contourf(Xd,Yd,a)
-#plt.colorbar()
+plt.errorbar(kvalues, overzre_fft_k, yerr = sigmazre, linestyle = 'None',capsize=4, marker ='o')
 #plt.legend()
 ax.set_xlabel(r'$k [Mpc^{-1}]$')
 ax.set_ylabel(r'$\delta_zre$ (k)')
 plt.title(r'$\delta_zre$ (k) as a function of k ')
 plt.show()
 
-overzre_fft_k = overzre_fft_k[1:]
-overd_fft_k = overd_fft_k[1:]
+
 #prim_basis = pymks.PrimitiveBasis(n_states=2)
 #X_ = prim_basis.discretize(overzre_fft_k)
-P_zz = abs(overzre_fft_k)**2
-P_mm = abs(overd_fft_k)**2
+
 b_mz = np.sqrt(np.divide(overzre_fft_k,overd_fft_k))
+bmz_errors = sa.compute_bmz_error(b_mz,overzre_fft_k,overd_fft_k,sigmad,sigmazre)
 #bmz = sa.compute_bmz(overzre_fft_k[1:],overd_fft_k[1:])
-radii = radii[1:]
+#radii = radii[1:]
 print(b_mz)
 
 #take out nan values in radii
-radii= radii[:len(b_mz)]
-nan_array = np.isnan(b_mz)
-not_nan_array = ~ nan_array
-bmz = b_mz[not_nan_array]
-radii = radii[not_nan_array]
+
+#nan_array = np.isnan(b_mz)
+#not_nan_array = ~ nan_array
+#bmz = b_mz[not_nan_array]
+#radii = radii[not_nan_array]
 
 fig, ax = plt.subplots()
 #plt.plot(overd_fft_k[1:], y_plot_fit)
-plt.errorbar(radii, b_mz, label = 'data fitting for',linestyle = 'None',capsize=4, marker ='o') #xerr = sigmad[2:], yerr = sigmazre[2:],
+plt.errorbar(kvalues, b_mz, label = 'data fitting for', linestyle = 'None',capsize=4, marker ='o') #xerr = sigmad[2:], yerr = sigmazre[2:], yerr = np.sqrt(bmz_errors)
 plt.title(r'$b_{zm}$ as a function of k ')
 ax.set_ylabel(r'$b_{mz}$ ')
 ax.set_xlabel(r'k')
 plt.show()
 
-errs = np.ones_like(b_mz)*0.2
+errs = np.ones_like(b_mz)*0.05
 #initialize the MCMC
 num_iter = 5000
-ndim = 3 # number of parameters
+ndim = 4 # number of parameters to fit for
 nwalkers = 32
-initial_pos = np.array((-0.5, 0.9, -14.8) + 2 * np.random.randn(nwalkers, ndim))
+initial_pos = np.array((6.4, 1.7, 0.4, 0.4) + 0.1 * np.random.randn(nwalkers, ndim))
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, sa.log_post_bmz, args=(radii, b_mz, errs))
+sampler = emcee.EnsembleSampler(nwalkers, ndim, sa.log_post_bmz, args=(kvalues, b_mz, errs))
 sampler.run_mcmc(initial_pos, num_iter, progress=True);
 
 flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
 fig = corner.corner(flat_samples, quantiles=[0.16, 0.5, 0.84])
 inds = np.random.randint(len(flat_samples), size=100)
 
-x0 = np.linspace(0, 25, 13)
+x0 = np.linspace(0, 1, 13)
 f, ax = plt.subplots(figsize=(6,4))
 for ind in inds:
     sample = flat_samples[ind]
-    ax.plot(x0, sample[0] * x0 + sample[1], alpha=0.05, color='red')
-ax.scatter(radii[:len(bmz)], bmz, marker ='o')
+    ax.plot(x0, (sample[1]/(1+(x0/sample[2]))**sample[0])+sample[3], alpha=0.05, color='red')
+ax.scatter(kvalues, b_mz, marker ='o')
 #ax.set_xlim(0, 10.)
 ax.set_ylabel(r'$\delta_zre$ (k) ')
 ax.set_xlabel(r'$\delta_m$ (k)')
 plt.show()
 
 #these lines represents curve fitting with scipy's weird algorithm
-"""
+
 # print(freqs[25:], division[25:])
-a1, b = sa.get_param_value(overd_fft_k[1:], overzre_fft_k[1:])
+a1, b = sa.get_param_value(kvalues, b_mz)
 a0,b0,k0 = a1[0:]
 print(a0, b0, k0)
-y_plot_fit = sa.lin_bias(overd_fft_k[1:], a0,b0,k0)
+y_plot_fit = sa.lin_bias(b_mz, a0,b0,k0)
 
 fig, ax = plt.subplots()
-#plt.plot(overd_fft_k[1:], y_plot_fit)
-plt.errorbar(overd_fft_k[1:], overzre_fft_k[1:], xerr = sigmad[1:], yerr = errs[1:], label = 'data fitting for',linestyle = 'None',capsize=4, marker ='o')
+plt.plot(kvalues, y_plot_fit)
+plt.errorbar(kvalues, b_mz, label = 'data fitting for',linestyle = 'None',capsize=4, marker ='o')
 plt.title(r'best curve fitting for the linear bias')
 plt.show()
-"""

statistical_analysis.py
@@ -20,6 +20,53 @@ from scipy.optimize import curve_fit
 import math as m
 #import pymks
 
+def average_overk(box_dim,field, radius_thick):
+    '''
+
+    :param box_dim:
+    :type box_dim:
+    :param field:
+    :type field:
+    :param radius_thick:
+    :type radius_thick:
+    :return:
+    :rtype:
+    '''
+
+    for i in tqdm(range(box_dim), 'transfering fields into k 1D array'):
+        for j in range(box_dim):
+            for z in range(box_dim):
+                k_radius = np.sqrt((i - cx) ** 2 + (j - cy) ** 2 + (z - cy) ** 2)
+                for step, radius in enumerate(radii):
+                    if k_radius < radius:
+                        count[(step)] += 1
+                        count_overd[(step)] += 1
+                        values[(step)] += overzre_fft[i, j, z]
+                        values_overd[(step)] += overd_fft[i, j, z]
+                        break
+
+
+def compute_bmz_error(b_mz, overzre_fft_k,overd_fft_k, sigmad, sigmazre):
+    '''
+    This module computes the errors in the bm_z error with error propagation from the average std
+    :param b_mz: the linear bias factor
+    :type b_mz: 1d array
+    :param overzre_fft_k: the average over phi and theta of the over_density field
+    :type overzre_fft_k: 1d array
+    :param overd_fft_k: the average over phi and theta of the over_redshift of reionization field
+    :type overd_fft_k: 1d array
+    :param sigmad: the std of the averaged over-density values
+    :type sigmad: 1d array
+    :param sigmazre: the std of the averaged over-redshift values
+    :type sigmazre: 1d array
+    :return: the errors bars for the bmz factor
+    :rtype: 1d array
+    '''
+    term1 = np.divide((0.5*sigmad),overd_fft_k)**2
+    term2 = np.divide((0.5*sigmazre),overzre_fft_k)**2
+    return np.multiply(b_mz,(np.sqrt(term1 + term2)))
+
+
 def lin_bias(x, a,b0,k0):
     '''
     This function represents the linear bias equation that will be fitted.
@@ -115,8 +162,8 @@ def log_post_lin(theta, x, y, yerr):
 
 
 def log_prior_bmz(theta):
-    a, b0, k0 = theta
-    if -10 < a < 10. and -20 < b0 < 20 and -40 < k0 < 20 :
+    a, b0, k0, h = theta
+    if 1 < a < 25. and 0.1 < b0 < 20 and 0.01 < k0 < 2 and h>0.2:
         return 0.0
     return -np.inf
 
@@ -134,10 +181,10 @@ def log_likelihood_bmz(theta, x, y, yerr):
     :return:
     :rtype:
     '''
-    a, b0, k0 = theta
+    a, b0, k0, h = theta
     sigma2 = yerr ** 2
     try:
-        model = b0/(1+x/k0)**a
+        model = (b0/(1+(x/k0))**a)+h
         likelihood = -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
         return likelihood
     except:

z_re_field.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 from scipy.stats import multivariate_normal
 
 
-def generate_zre_field(max_z, min_z, z_shift, HII_Dim):
+def generate_zre_field(max_z, min_z, z_shift, HII_Dim, box_len):
     """
     This function generate a z_re field with coeval cubes at different reionization redshift.
     :param max_z: [float] the maximum redshift of the plot
@@ -26,7 +26,7 @@ def generate_zre_field(max_z, min_z, z_shift, HII_Dim):
     #creating a new cube where reionization vener occured (-1)
     final_cube = np.full((HII_Dim, HII_Dim, HII_Dim), -1)
     for redshift in tqdm(np.arange(min_z, max_z, z_shift), 'computing the redshift of reionization'):
-        new_cube = p21c.run_coeval(redshift=redshift,user_params={'HII_DIM': HII_Dim, "USE_INTERPOLATION_TABLES":False}).z_re_box
+        new_cube = p21c.run_coeval(redshift=redshift,user_params={'HII_DIM': HII_Dim, 'BOX_LEN': box_len, "USE_INTERPOLATION_TABLES":False}).z_re_box
         final_cube[new_cube > -1] = new_cube[new_cube > -1]
     return final_cube
 